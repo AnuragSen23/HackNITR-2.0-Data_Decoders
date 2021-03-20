60ac0b7071db73d4a650c924cd768fdce18f2850
Anurag Sen <71377356+AnuragSen23@users.noreply.github.com>
2021-03-20 13:46:26 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,9 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-[general]\n\
-email = “your-email@domain.com”\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
 [server]\n\
-headless = true\n\
-enableCORS=false\n
 port = $PORT\n\
-[theme]\n\
-primaryColor="#08424f"\n\
-backgroundColor="#08424f"\n\
-secondaryBackgroundColor="#0d5a6a"\n\
-textColor="#ffffff"\n\
-font=“sans serif”\n\
+enableCORS = false\n\
+headless = true\n\
+\n\
 " > ~/.streamlit/config.toml
